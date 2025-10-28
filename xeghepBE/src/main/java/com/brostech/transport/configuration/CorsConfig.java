49150cfc72935302a6dac9c71772b6035fddc1ae
halo1995 <nguyennhuong.act@gmail.com>
2025-10-28 07:50:13 +0700

    dsdsds

--- xeghepBE/src/main/java/com/brostech/transport/configuration/CorsConfig.java
+++ xeghepBE/src/main/java/com/brostech/transport/configuration/CorsConfig.java
@@ -1,28 +1,28 @@
-package com.brostech.transport.configuration;
+// package com.brostech.transport.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "https://xeghepnghiaphat.io.vn", "https://www.xeghepnghiaphat.io.vn" ));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setExposedHeaders(Arrays.asList("*"));
+//     @Bean
+//     public CorsFilter corsFilter() {
+//         CorsConfiguration config = new CorsConfiguration();
+//         config.setAllowCredentials(true);
+//         config.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "https://xeghepnghiaphat.io.vn", "https://www.xeghepnghiaphat.io.vn" ));
+//         config.setAllowedHeaders(Arrays.asList("*"));
+//         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//         config.setExposedHeaders(Arrays.asList("*"));
         
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", config);
         
-        return new CorsFilter(source);
-    }
-}
+//         return new CorsFilter(source);
+//     }
+// }
